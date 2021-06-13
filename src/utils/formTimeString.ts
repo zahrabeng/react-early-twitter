78214020d3b946e7188a4098731449af2ba14133
Richard Ng <richardcrng@gmail.com>
2021-06-13 20:53:34 +0100

    Document formTimeString

--- src/utils/formTimeString.ts
+++ src/utils/formTimeString.ts
@@ -2,6 +2,12 @@ import removeFinalCharacter from "./removeFinalCharacter";
 
 export type TimeUnit = "minutes" | "hours" | "days";
 
+/**
+ * Generate a string representing the amount of time
+ * @param count how many of the time unit there are
+ * @param unit the unit of time
+ * @returns a formatted time string
+ */
 function formTimeString(count: number, unit: TimeUnit): string {
   // remove the final character from unit if count is exactly 1
   const compensatedUnit = count === 1 ? removeFinalCharacter(unit) : unit
